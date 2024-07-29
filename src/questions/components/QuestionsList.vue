8d6773f785a685f8ca80a4a42a915739af476de0
SergeyOberemok <sergey.oberemok@gmail.com>
2024-07-29 18:30:05 +0200

    [feat] Added fetching question. Removed unnecessary boilerplates. Added proxy

---
+++ src/questions/components/QuestionsList.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  questions: {
+    type: Array,
+    required: true
+  }
+})
+
+const questions = props.questions
+</script>
+
+<template>
+  <div class="wrapper">
+    <div v-for="question in questions" :key="question.id">{{ question.description }}</div>
+  </div>
+</template>
---
+++ src/questions/shared/question.model.js
@@ -0,0 +1,6 @@
+export class Question {
+  constructor({ id, description }) {
+    this.id = id
+    this.description = description
+  }
+}
