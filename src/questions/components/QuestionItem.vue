96f6236b9d36226c71b32c10319e6f4f65856acb
SergeyOberemok <sergey.oberemok@gmail.com>
2024-08-01 16:03:25 +0200

    [feat] Added notes field

--- src/questions/components/QuestionItem.vue
+++ src/questions/components/QuestionItem.vue
@@ -13,10 +13,14 @@ const props = defineProps({
       <pre class="whitespace-pre text-sm">{{ answer }}</pre>
     </div>
 
-    <div v-for="(label, index) in question.labels" :key="index">
-      <span class="text-sm font-medium text-white bg-blue-400 rounded-md px-2 py-1">{{
-        label
-      }}</span>
+    <div class="mb-3">{{ question.notes }}</div>
+
+    <div class="flex">
+      <div v-for="(label, index) in question.labels" :key="index" class="me-1">
+        <span class="text-sm font-medium text-white bg-blue-400 rounded-md px-2 py-1">{{
+          label
+        }}</span>
+      </div>
     </div>
   </div>
 </template>
